cc744bf285d97f40778f1c8e72e57f93a9ee3cfc
weeloong93 <weeloong.ng@u.nus.edu>
2016-06-17 12:22:33 +0800

    Revert "Reverting back"
    This reverts commit 6676f701e0528a8d1d1ff5071052000dd22daf6a.

--- Freeberg/stocktracker/models.py
+++ Freeberg/stocktracker/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Stock(models.Model):
+    company_name = models.CharField(max_length = 100)
     ticker = models.CharField(max_length=10)
+    bloom_ticker = models.CharField(max_length=10, null=True)
     lastprice = models.CharField(max_length = 10000000, default ='-')
-    open = models.FloatField(null=True)
-    close = models.FloatField(null=True)
-    volume = models.IntegerField(null=True)
+    open = models.FloatField(blank=True)
+    close = models.FloatField(blank=False)
+    volume = models.IntegerField(blank=True)
+    description = models.TextField(null=True)
+    start_date = models.DateField(null=True)
 
     def __str__(self):
         return self.ticker
